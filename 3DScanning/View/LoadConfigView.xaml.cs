28a003e2a0fadf2e905c0c5cc276c7369180b6bb
noa26 <60259755+noa26@users.noreply.github.com>
2020-08-14 01:57:53 +0300

    GUI 0.2 added configuration views.
    Must fix navigation!
    must add logic!

---
+++ 3DScanning/View/LoadConfigView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+
+namespace _3DScanning.View
+{
+    /// <summary>
+    /// Interaction logic for LoadConfigView.xaml
+    /// </summary>
+    public partial class LoadConfigView : Page
+    {
+        public LoadConfigView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+    }
+}
---
+++ 3DScanning/View/SaveConfigView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+
+namespace _3DScanning.View
+{
+    /// <summary>
+    /// Interaction logic for SaveConfigView.xaml
+    /// </summary>
+    public partial class SaveConfigView : Page
+    {
+        public SaveConfigView()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, txtEditor.Text);
+        }
+    }
+}
